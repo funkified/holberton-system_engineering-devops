5f877571b77075b1770a2f46b44a74ae831cff14
Sergio Vera <sergio.vera@icloud.com>
2022-01-03 20:07:35 +0000

    fixing spacing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# file create using Puppet
+#file create using Puppet
 
 file { 'school':
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
