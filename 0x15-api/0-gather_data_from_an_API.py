2df8d5c0a9364d833c4aa49d9719001a5ae72a6a
Sergio Vera <sergio.vera@icloud.com>
2022-01-31 22:11:52 +0000

    format fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,12 +9,12 @@ from sys import argv
 if __name__ == '__main__':
 
     employee_id = argv[1]
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                        format(employee_id))
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(employee_id))
 
     name_employee = user.json()['name']
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                        format(employee_id))
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(employee_id))
     todo = todo.json()
 
     done_task = 0
@@ -25,8 +25,9 @@ if __name__ == '__main__':
             done_task += 1
             task_list.append(task['title'])
 
-    print("Employee {} is done with tasks({}/{}):".
-          format(name_employee, done_task, len(todo)))
+    print("Employee {} is done with tasks({}/{}):".format(name_employee,
+                                                          done_task,
+                                                          len(todo)))
 
     for done in range(len(task_list)):
         print("\t {}".format(task_list[done]))
