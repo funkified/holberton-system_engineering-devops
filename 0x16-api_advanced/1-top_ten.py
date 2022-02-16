99b033815451247940951c61801a3d15c63fb828
Sergio Vera <sergio.vera@icloud.com>
2022-02-16 03:37:48 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Scripts that GET Reddit API
+"""
+import json
+from requests import *
+
+
+def top_ten(subreddit):
+    """ GET the top ten of hot post on reddit """
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    agent = {'User-agent': 'funkified'}
+    req = get(url, headers=agent)
+    jreq = req.json()
+
+    if req.status_code == 400:
+        print('None')
+    else:
+        posts = jreq['data']['children']
+        print(posts[i]['data']['title'] for i in range(len(posts)))
