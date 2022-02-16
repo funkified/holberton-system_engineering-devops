1bae0765417e3a7eba213b0e8ebbf8f0d9fef9b5
Sergio Vera <sergio.vera@icloud.com>
2022-02-16 03:41:07 +0000

    fixing url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,13 +8,13 @@ from requests import *
 
 def top_ten(subreddit):
     """ GET the top ten of hot post on reddit """
-    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json=10'
     agent = {'User-agent': 'funkified'}
     req = get(url, headers=agent)
     jreq = req.json()
 
     if req.status_code == 400:
-        print('None')
+        print(None)
     else:
         posts = jreq['data']['children']
         print(posts[i]['data']['title'] for i in range(len(posts)))
